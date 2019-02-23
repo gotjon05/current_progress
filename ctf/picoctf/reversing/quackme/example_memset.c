f8a4e2e0a19364c5b4607bc5979ef16c650f3417
gotjon05 <gotjon05@localhost.localdomain>
2019-02-23 00:06:24 -0500

    feb23

--- ctf/picoctf/reversing/quackme/example_memset.c
+++ ctf/picoctf/reversing/quackme/example_memset.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
+
+//memset is copying the character  (an unsigned char) to the first n characters of the string pointed to, by the argument str.
+
+
+
 int main () {
    char str[50];
 
    strcpy(str,"This is string.h library function");
    puts(str);
+   //output-> This is string.h library function
 
 
+
+   //fills 7 characters starting from "str" with $
    memset(str,'$',7);
    puts(str);
-   
+   //output-> $$$$$$$ string.h library function 
    return(0);
+
 }
---
+++ ctf/picoctf/reversing/quackme/reverse_quack.c
@@ -0,0 +1,36 @@
+//var int local_1dh @ ebp-0x1d       
+//var int local_1ch @ ebp-0x1c       
+//var int local_18h @ ebp-0x18       
+//var int local_14h @ ebp-0x14 --- string count       
+//var int local_10h @ ebp-0x10       
+//var int local_ch @ ebp-0xc         
+
+
+
+#include <iostream> 
+
+int do_magic()
+{
+	int len = 25;
+
+
+
+	var_14 = getline(
+
+
+
+
+
+
+
+	if(local_1ch != 19)
+	{
+		puts("You are winner");
+	}	
+
+	
+
+
+
+
+}
